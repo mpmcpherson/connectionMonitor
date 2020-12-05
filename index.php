1bec3c4d15f63072c7140ea41fa7b37e7cb6497c
Michael McPherson <michael@mpmcpherson.com>
2020-12-04 20:21:04 -0500

    Whacked together the site, the query, and a little formatting. I hate how js handles dates

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 
 <html>
@@ -23,11 +24,7 @@
 			<div class="header">
 				<h3>Page Title</h3>
 			</div> 
-			<!--<button id="btn">Top Button Text</button>-->
-			<div id="text">Here's some text, and isn't it interesting?</div>
-			<div id="text">Lorem ipsum dolor sit amet. Dulche et decorum est</div>
-			<div id="text">Here's some text, and isn't it interesting?</div>
-			<div> End Text		</div>	
+			
 		</div>
 
 	</div>
@@ -37,29 +34,51 @@
 <script type="text/javascript" src="js\helper.js"></script>
 <script type="text/javascript">
 	var button = document.getElementById('btn');
-	var resultDiv = document.getElementById('results');
+	var targetDiv = document.getElementById('body');
 
 	docReady(function() {
     	let leftbar = document.getElementById('leftBar');
-    	leftbar.setAttribute("class","leftBar full")
-	});
+    	leftbar.setAttribute("class","leftBar full");
 
- 
-	button.addEventListener('click', function() {
-		//console.log("hit click");
-		alert("click");
-  		/*getRequest('basicGenerator.php',
+    	getRequest('resources/resultsRequest.php',
   			function(response){
+  				
   				//console.log(response);
-  				resultDiv.innerHTML = response;
+  				let composition = "<table><tr><th>ID</th><th>TIME</th><th>LOCATION</th></tr>";
+  				try{
+  					let v = JSON.parse(response);
+  					console.log(v);
+  					
+  					for(let i = 0; i < v.length; i++)
+  					{  					
+						let outDate = new Date(Date.parse(v[i][1]));
+						
+						composition += "<tr><td>"+v[i][0]+"</td><td>"+outDate.toString()+"</td><td>"+v[i][2]+"</td></tr>";
+  					}
+  				}catch(error)
+  				{
+  					console.log(error);
+  				}
+  				//targetDiv.innerHTML = response;
+  				composition += "</table>";
+
+  				let carrierDiv = document.createElement("div");
+  				carrierDiv.style.width='100%';
+  				carrierDiv.innerHTML = composition;
+  				
+  				targetDiv.appendChild(carrierDiv);
   			},
   			function(response){
-  				resultDiv.innerHTML = 'An error occurred during your request: ' +  response.status + ' ' + response.statusText;
+  				targetDiv.innerHTML = 'An error occurred during your request: ' +  response.status + ' ' + response.statusText;
   			},
   			null);
-*/
+
+
 	});
 
+ 	
+
+		
 
 
 </script>
---
+++ resources/resultsRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+//in another php file, get a handle to the results of mysqli_connect. 
+//In this case, it's literally named $dbhandle (see below).
+require 'defaultConnector.php';
+
+$qq = "SELECT * FROM connTime ORDER BY Id desc";
+    
+
+$result = mysqli_query($dbhandle, $qq);
+
+$output = array();
+
+//fetch tha data from the database
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+
+
+    array_push($output, array($row['id'], date_format(date_create($row['time']),"m/d/Y h:i:s A"), $row['location']));
+
+}
+
+echo json_encode($output);
+
+//var_dump($result);
+?>
+
